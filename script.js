453cc0aeec5d9ae17c7b8b4736b4ec489f0e02e0
josh-conde <joshconde@aol.com>
2025-01-11 22:50:51 -0500

    Major changes to page

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.getElementsByClassName("navbar");
-  
+
     navbar.classList.add("fade-in");
   });
+
+const projectCards = document.querySelectorAll('.card');
+
+projectCards.forEach(card => {
+    card.addEventListener('click', () => {
+        const description = card.querySelector('.hidden');
+        if (description) {
+            description.classList.toggle('visible');
+            card.classList.toggle('fade-background');
+        }
+    });
+});
